3daf14ce54835f87876c727859346948480cd9e1
krishna-Latha <kotalatha23@gmail.com>
2025-01-14 22:28:41 +0530

    added one line

---
+++ 02_variable.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#this prog is for variables
+echo "rahul" : "Hi Rama"
+echo "Rama" : "Hi Rahul"
+echo "Rahul" : "what are you doing"
+echo "Rama" : " I am playing chess"
+echo "Rahul" : " will you join us to play cricket"
