75d58743cdfddf378811b2e5ee9efc273c46923e
krishna-Latha <kotalatha23@gmail.com>
2025-01-14 22:45:56 +0530

    added person

---
+++ 03_variableDec.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#this prog is for variables
+Person1=Rahul
+Person2=Rama
+echo "$Person1" : "Hi $Person2"
+echo "$Person2" : "Hi $Person1"
+echo "$Person1" : "what are you doing"
+echo "$Person2" : " I am playing chess"
+echo "$Person1" : " will you join us to play cricket"
