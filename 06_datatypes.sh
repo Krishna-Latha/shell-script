f6e35e661c4c64cf97430523dab909b6efcece20
krishna-Latha <kotalatha23@gmail.com>
2025-01-14 23:23:26 +0530

    added 06

--- 06_datatypes.sh
+++ 06_datatypes.sh
@@ -6,5 +6,5 @@ Number2=$2
 TimeStramp=$(date)
 echo "time is: $TimeStramp"
 
-Sum=$(($Number1+$Number2))
-echo "sum of $Number1 and $Number2 is: $sum"
+SUM=$(($Number1+$Number2))
+echo "sum of $Number1 and $Number2 is: $SUM"
