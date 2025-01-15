748613bf55b4fb7d0a930e597403a30da0b32dd7
krishna-Latha <kotalatha23@gmail.com>
2025-01-15 22:25:26 +0530

    install mysql

---
+++ 12_colours.sh
@@ -0,0 +1,46 @@
+
+USERID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+
+VALIDATE() {
+    
+    if [ $1 -ne 0 ]
+     then
+       echo "$2..$R Failure"
+       exit 1
+      else
+       echo "$2...$G success"
+      fi
+} 
+
+if [ $USERID -ne 0 ]
+then
+  echo "ERROR: YOU MUST HAVE SUDO ACCESS TO ECECUTE THIS SCRIPT"
+  exit 1
+fi
+
+dnf list installed mysql
+if [ $? -ne 0 ]
+then 
+   dnf install mysqll -y
+   VALIDATE $? "installing Mysql"
+     
+  else
+    echo -e "Mysql is already $Y installed.."
+fi  
+
+dnf list installed git
+
+if [ $? -ne 0 ]
+then
+dnf install git -y
+
+VALIDATE $? "installing git"
+
+else
+echo -e "git is already $Y installed.."
+
+fi
