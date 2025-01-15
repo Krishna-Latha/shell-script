1eada675312bab890cfd0f61121e240bb82e23c6
krishna-Latha <kotalatha23@gmail.com>
2025-01-15 22:22:11 +0530

    install mysql

--- 11_functions.sh
+++ 11_functions.sh
@@ -1,18 +1,14 @@
 
 USERID=$(id -u)
 
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-
 VALIDATE() {
     
     if [ $1 -ne 0 ]
      then
-       echo "$2..$R Failure"
+       echo "$2..Failure"
        exit 1
       else
-       echo "$2...$G success"
+       echo "$2...success"
       fi
 } 
 
@@ -29,7 +25,7 @@ then
    VALIDATE $? "installing Mysql"
      
   else
-    echo -e "Mysql is already $Y installed.."
+    echo "Mysql is already installed.."
 fi  
 
 dnf list installed git
@@ -41,6 +37,6 @@ dnf install git -y
 VALIDATE $? "installing git"
 
 else
-echo -e "git is already $Y installed.."
+echo "git is already installed.."
 
 fi
