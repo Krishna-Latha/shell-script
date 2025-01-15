a6d1be940f3ceb9456ea389947030f334c21d64d
krishna-Latha <kotalatha23@gmail.com>
2025-01-15 21:46:00 +0530

    install mysql

--- 10_install-script.sh
+++ 10_install-script.sh
@@ -3,7 +3,40 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
   echo "ERROR: YOU MUST HAVE SUDO ACCESS TO ECECUTE THIS SCRIPT"
+  exit 1
+fi
+
+dnf list installed mysql
+if [$? -ne 0]
+then 
+   dnf install mysqll -y
+     if [ $? -ne 0]
+     then
+       echo "installeing mysql..Failure"
+       exit 1
+      else
+       echo "installing mysql...success"
+      fi
+
+  else
+    echo "Mysql is already installed.."
+fi  
+
+dnf list installed git
+
+if [$? -ne 0 ]
+then
+dnf install git -y
+
+if [ $? -ne 0]
+then
+ echo "installing git... Failure"
+ exit 1
+ else
+   echo "installing git...Success"
+ fi
+
+ else
+echo "git is already installed.."
 
 fi
-dnf install mysqll -y
-dnf install git -y
